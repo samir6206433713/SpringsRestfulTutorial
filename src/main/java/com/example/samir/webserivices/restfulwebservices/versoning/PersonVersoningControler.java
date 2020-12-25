28d3740f006eef420d4d4d3cb6aaaa750fefde9f
samir <samir.softians@gmail.com>
2020-12-25 23:27:37 +0530

    authetication logic

--- src/main/java/com/example/samir/webserivices/restfulwebservices/versoning/PersonVersoningControler.java
+++ src/main/java/com/example/samir/webserivices/restfulwebservices/versoning/PersonVersoningControler.java
@@ -10,8 +10,66 @@ public class PersonVersoningControler {
     public Person1 person1(){
         return new Person1("b");
     }
+
+
     @GetMapping("v2/person")
     public Person2 person2(){
         return new Person2(new Name("c","d"));
     }
+
+
+
+
+    @GetMapping(value = "/person/param",params = "version=1")
+    public Person1 personv1(){
+        return new Person1("b");
+    }
+
+
+    @GetMapping(value = "/person/param",params = "version=2")
+    public Person2 personv2(){
+        return new Person2(new Name("c","d"));
+    }
+
+
+
+
+
+    @GetMapping(value = "/person/header",headers = "X-API-VERSION=1")
+    public Person1 headerv1(){
+        return new Person1("b");
+    }
+
+
+    @GetMapping(value = "/person/header",headers = "X-API-VERSION=2")
+    public Person2 headerv2(){
+        return new Person2(new Name("c","d"));
+    }
+
+
+
+//---------------------this did not work ---------------------------
+
+    /*@GetMapping(value = "/person/produces",produces = "application-vnd.company.app-v1+json")
+    public Person1 producesv1(){
+        return new Person1("b");
+    }
+
+
+    @GetMapping(value = "/person/produces",produces = "application-vnd.company.app-v2+json")
+    public Person2 producesv2(){
+        return new Person2(new Name("c","d"));
+    }*/
+
+
+//---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 }
